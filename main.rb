25b342bd2baa5acdae1873d22dcbb01aba62e262
rotosiri-zu <hongjitufang25@gmail.com>
2021-07-02 23:26:43 +0900

    first commit

---
+++ main.rb
@@ -0,0 +1,54 @@
+class Brave
+  # nameのセッター
+  def name=(name)
+    @name = name
+  end
+
+  # nameのゲッター
+  def name
+    @name
+  end
+
+  # hpのセッター
+  def hp=(hp)
+    @hp = hp
+  end
+
+  # hpのセッター
+  def hp
+    @hp
+  end
+
+  # offenseのセッター
+  def offense=(offense)
+    @offense = offense
+  end
+  
+  # offenseのゲッター
+  def offense
+    @offense
+  end
+
+  # defenseのセッター
+  def defense=(defense)
+    @defense = defense
+  end
+
+  # defenseのゲッター
+  def defense
+    @defense
+  end
+end
+
+brave = Brave.new
+brave.name = "テリー"
+brave.hp = 500
+brave.offense = 150
+brave.defense = 100
+
+puts <<~TEXT
+NAME:#{brave.name}
+HP:#{brave.hp}
+OFFENSE:#{brave.offense}
+DEFENSE#{brave.defense}
+TEXT
