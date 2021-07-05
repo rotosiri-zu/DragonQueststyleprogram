dfaf888d4459f528dd6ac717f6400181257d9a58
rotosiri-zu <hongjitufang25@gmail.com>
2021-07-05 22:38:08 +0900

    1-6 アクセスメソッド

--- main.rb
+++ main.rb
@@ -1,6 +1,11 @@
 class Brave
 
-  
+  #attr_readerの記載でゲッターを省略することができる
+  # 複数の値を同時に指定することができる
+  attr_reader :name, :hp, :offense, :defense
+  # attr_writerでセッターを定義
+  # セッターゲッターを一括定義
+  attr_accessor :hp
   # initializeメソッドを定義
   #paramsで一括で受け取る
   #引数に**を記載:ハッシュしか受け取れなくなる
@@ -12,25 +17,6 @@ class Brave
     @defense = params[:defense]
   end
 
-  # nameのゲッター
-  def name
-    @name
-  end
-
-  # hpのセッター
-  def hp
-    @hp
-  end
-  
-  # offenseのゲッター
-  def offense
-    @offense
-  end
-
-  # defenseのゲッター
-  def defense
-    @defense
-  end
 end
 
 brave = Brave.new(name:"テリー", hp:500, offense:150, defense:100)
@@ -41,3 +27,7 @@ HP:#{brave.hp}
 OFFENSE:#{brave.offense}
 DEFENSE#{brave.defense}
 TEXT
+
+brave.hp -= 30
+
+puts "#{brave.name}はダメージを受けた！残りは#{brave.hp}だ"
