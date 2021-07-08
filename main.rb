7b5c05f7675d4e20ba852444add61b0ea7d1e561
rotosiri-zu <hongjitufang25@gmail.com>
2021-07-08 23:45:57 +0900

    3-4 勇者クラスの攻撃処理の切り分け

--- main.rb
+++ main.rb
@@ -29,30 +29,66 @@ class Brave
    
    puts "#{@name}の攻撃"
 
-   #0~3の間でランダムに数字が変わる
-   attack_num = rand(4)
+   # decision_attack_typeメソッドの呼び出し
+   attack_type = decision_attack_type
 
-   #4分の1の確率でspecial_attackを実行
-   if attack_num == 0
-    # 必殺攻撃の表示
-    puts "必殺攻撃"
-    # calculate_special_attackの呼び出し
-    # 攻撃力の1.5倍の数値が戻り値として返ってくる
-    damage = calculate_special_attack - monster.defense
+   # calculate_damageメソッドの呼び出し
+   # キーワード引数を設定
+   damage = calculate_damage(target:monster, attack_type:attack_type)
+
+   # ダメージをHPに反映させる
+   # キーワード引数を設定
+   cause_damage(target:monster, damage:damage)
+
+    # メッセージを追記
+    # puts "#{monster.name}は#{damage}のダメージを受けた"
+    puts "#{monster.name}の残りHPは#{monster.hp}だ"
+  end
+
+  # ここから下のメソッドをprivateメソッドにする
+  private
+  # 攻撃の種類（通常攻撃 or 必殺攻撃）を判定するメソッド
+  def decision_attack_type
+    #0~3の間でランダムに数字が変わる
+    attack_num = rand(4)
+  
+    if attack_num == 0
+      puts "必殺攻撃"
+      "special_attack"
+    else
+      puts "通常攻撃"
+      "normal_attac"
+    end
+  end
+
+  # ダメージの計算メソッド
+  # **paramsで受け取る
+  def calculate_damage(**params)
+    # 変数に格納することによって将来ハッシュのキーに変更があった場合でも変更箇所が少なくて済む
+    target = params[:target]
+    attack_type = params[:attack_type]
+
+    # attack_typeを用いて攻撃処理を振り分け
+   if attack_type == "special_attack"
+    calculate_special_attack - monster.defense
    else
-    # 通常攻撃の表示
-    puts "通常攻撃"
-    damage = @offense - monster.defense
+    @offense - monster.defense
    end
-   # ダメージ計算の処理を追加
-  # damage = @offense - monster.defense
-   # モンスターのhpから計算したダメージを引く
-   # 自己代入：monster.hpからdamageを引いた値をmonster.hpに代入
-   monster.hp -= damage
+  end
 
-   # メッセージを追記
-   puts "#{monster.name}は#{damage}のダメージを受けた"
-   puts "#{monster.name}の残りHPは#{monster.hp}だ"
+  # HPにダメージを反映させる
+  # **paramsで受け取る
+  def cause_damage(**params)
+    # 変数に格納することによって将来ハッシュのキーに変更があった場合でも変更箇所が少なくて済む
+    damage = params[:damage]
+    target = params[:target]
+
+    # ダメージ計算の処理を追加
+    # damage = @offense - monster.defense
+    # モンスターのhpから計算したダメージを引く
+    # 自己代入：monster.hpからdamageを引いた値をmonster.hpに代入
+    monster.hp -= damage
+    puts "#{monster.name}は#{damage}のダメージを受けた"
   end
 
   def calculate_special_attack
@@ -61,6 +97,7 @@ class Brave
     # 攻撃力が1.5倍
     @offense * SPECIAL_ATTACK_CONSTANT
   end
+
 end
 
 class Monster
@@ -114,7 +151,7 @@ class Monster
 
     # クラス外から呼び出せないようにする
     private
-
+    
     # 変身メソッドの定義
     def transform
       # 変身後の名前
@@ -138,7 +175,6 @@ class Monster
   brave = Brave.new(name:"テリー", hp:500, offense:150, defense:100)
   # ハッシュ形式でデータを渡すのでどういうデータを渡しているのか把握しやすくなる
   monster = Monster.new(name:"スライム", hp:250, offense:200, defense:100)
-  
   
   brave.attack(monster)
   monster.attack(brave)
