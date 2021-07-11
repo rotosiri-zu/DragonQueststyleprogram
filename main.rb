f2550690a414aa7c4c03b1c895c1b1c252af71cf
rotosiri-zu <hongjitufang25@gmail.com>
2021-07-11 22:38:14 +0900

    4-2 キャラクタークラスのファイル分割

--- main.rb
+++ main.rb
@@ -1,14 +1,4 @@
-class Character
-  attr_reader :offense, :defense
-  attr_accessor :hp, :name
-
-  def initialize(**params)
-    @name = params[:name]
-    @hp = params[:hp]
-    @offense = params[:offense]
-    @defense = params[:defense]
-  end
-end
+require './character'
 
 class Brave < Character
 
