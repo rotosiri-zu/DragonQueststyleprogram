60451e4d5d34cec36f9a1a377a639daf2c50743d
rotosiri-zu <hongjitufang25@gmail.com>
2021-07-14 22:57:14 +0900

    4-5 games_controller.rbのリファクタリング

--- games_controller.rb
+++ games_controller.rb
@@ -5,47 +5,70 @@ class GamesController
 
   # バトルの処理
   def battle(**params)
-    brave = params[:brave]
-    monster = params[:monster]
+   # build_charactersを呼びだし
+   build_characters(params)
 
     loop do
-      brave.attack(monster)
+      # インスタンス変数に変更
+      brave.attack(@monster)
 
       # モンスターのHPが0以下になったら無限ループを終了させる
       # 1行で記述
-      break if battle_end?(monster)
+      break if battle_end? #引数が必要なくなる
 
-      monster.attack(brave)
+      # インスタンス変数に変更
+      monster.attack(@brave)
       # 勇者のHPが0以下になったら無限ループを終了させる
       # 1行で記述
-      break if battle_end?(brave)
+      break if battle_end? #引数が必要なくなる
     end
 
-    if battle_result(brave)
-     result = calculate_of_exp_and_gole(monster)
-      puts "#{brave.name}はたたかいに勝った"
-      puts "#{result[:exp]}の経験値と#{result[:gold]}ゴールドを獲得した"
-    else
-      puts "#{brave.name}は戦いに負けた"
-      puts "目の前が真っ暗になった"
-    end
+    # battle_judgmentメソッドを呼び出す
+    battle_judgment
   end
 
 # 以下のメソッドはクラス外から呼び出す必要がないのでprivate以下に記述する
 private
 
+  # キャラクターのインスタンスをインスタンス変数に格納
+  def build_characters(**params)
+    # 勇者クラス、モンスタークラスそれぞれのインスタンスをインスタンス変数に代入
+    @brave = params[:brave]
+    @monster = params[:monster]
+  end
+
   # バトル終了の判定
-  def battle_end?(character)
-    character.hp <= 0
+  # 引数が必要なくなる
+  def battle_end?
+    # 勇者かモンスター、どちらかのHPが0になったらバトルが終了する
+    @brave.hp <= 0 || @monster.hp <= 0
   end
 
   # 勇者の勝利判定
-  def battle_result(brave)
+  # 引数が必要なくなる
+  # brave_win?にメソッド名を変更
+  def battle_win?
+    # インスタンス変数に変更
     brave.hp > 0
   end
 
+  # 勇者の勝敗によりメッセージを変更する
+  def battle_judgment
+    # brave_win?を呼び出す
+    if brave_win? #引数が必要なくなる
+      result = calculate_of_exp_and_gole #引数が必要なくなる
+      # インスタンス変数に変更
+      puts "#{@brave.name}はたたかいに勝った"
+      puts "#{result[:exp]}の経験値と#{result[:gold]}ゴールドを獲得した"
+    else
+      # インスタンス変数に変更
+      puts "#{@brave.name}は戦いに負けた"
+      puts "目の前が真っ暗になった"
+    end
+  end
   # 経験値とゴールドの計算
-  def calculate_of_exp_and_gole(monster)
+  # 引数が必要なくなる
+  def calculate_of_exp_and_gole
     exp = (monster.offense + monster.defense) * EXP_CONSTANT
     gole = (monster.offense + monster.defense) * GOLD_CONSTANT
     result = {exp:exp,gold:gole}
---
+++ message_dialog.rb
@@ -0,0 +1,13 @@
+module MessageDialog
+  # 攻撃時のメッセージ
+  def attack_message
+  end
+
+  # ダメージを受けた時のメッセージ
+  def damage_message
+  end
+
+  # バトルが終了した時のメッセージ
+  def end_message
+  end
+end
